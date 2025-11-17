fa4d5488a7138e5ae18b364134ec9e5baa3246bb
Wiktor Bramer <bramerwiktor@gmail.com>
2025-11-17 18:13:43 +0100

    CoreData setup

---
+++ BudgetManager/ViewModels/ExpenseViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ExpenseViewModel.swift
+//  BudgetManager
+//
+//  Created by Wiktor Bramer on 25/10/2025.
+//
+
+import Foundation
+import CoreData
+
+class ExpenseViewModel {
+    
+    
+    
+}
