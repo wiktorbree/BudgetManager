bf2132c400dd902b003898d5350aec3221408030
Wiktor Bramer <bramerwiktor@gmail.com>
2025-11-17 19:36:22 +0100

    ExpenseView

---
+++ BudgetManager/Views/AddExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpenseView.swift
+//  BudgetManager
+//
+//  Created by Wiktor Bramer on 17/11/2025.
+//
+
+import SwiftUI
+
+struct AddExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExpenseView()
+}
