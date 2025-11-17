bb53b86e298a1faf1abcf5b730127bf0c4024a26
Jakub <k.bugalski04@gmail.com>
2025-11-17 20:22:25 +0100

    wypisane dane w content view(chyba)

---
+++ BudgetManager/Views/test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  BudgetManager
+//
+//  Created by Mac-Jakub on 17/11/2025.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test()
+}
